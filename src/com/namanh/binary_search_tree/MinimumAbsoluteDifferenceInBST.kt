44ae867875ee32ef25e75954b27df489eeca46b0
Henry <namanh11611@gmail.com>
2023-02-17 23:41:19 +0700

    Solve Minimum Absolute Difference in BST

---
+++ src/com/namanh/binary_search_tree/MinimumAbsoluteDifferenceInBST.kt
@@ -0,0 +1,34 @@
+package com.namanh.binary_search_tree
+
+import com.namanh.common.TreeNode
+
+/**
+ * https://leetcode.com/problems/minimum-absolute-difference-in-bst
+ * Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different
+ * nodes in the tree.
+ *
+ * S1: Use inorder traversal, we can iterate tree in ascending order
+ * S2: Use prev variable to store previous value, calculate distance between prev and current node
+ *
+ * Time: O(n)
+ * Space: O(h) with h is height of tree
+ */
+object MinimumAbsoluteDifferenceInBST {
+    private var minDistance = Int.MAX_VALUE
+    private var prev: TreeNode? = null
+
+    fun getMinimumDifference(root: TreeNode?): Int {
+        inorder(root)
+        return minDistance
+    }
+
+    private fun inorder(node: TreeNode?) {
+        if (node == null) return
+        inorder(node.left)
+        prev?.let {
+            minDistance = minOf(minDistance, node.`val` - it.`val`)
+        }
+        prev = node
+        inorder(node.right)
+    }
+}
